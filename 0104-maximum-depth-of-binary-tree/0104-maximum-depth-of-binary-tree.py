96b2aa8c4ab640ce004222ecf709406ae3f111b8
Andre Ramos da Silva <andre-ramos1994@hotmail.com>
2023-10-13 01:38:38 -0300

    Time: 45 ms (65.04%), Space: 17.5 MB (94.23%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
@@ -9,7 +9,19 @@ class Solution:
         #BFS/DFS
         if root is None:
             return 0
-        left  = self.maxDepth(root.left)
-        right = self.maxDepth(root.right)
+        # left  = self.maxDepth(root.left)
+        # right = self.maxDepth(root.right)
 
-        return max(left,right) + 1
+        # return max(left,right) + 1
+        max_dpt =0 
+        stack = [(root,1)]
+        while stack:
+            curr_node , dpt = stack.pop(0)
+
+            max_dpt = max(max_dpt,dpt)
+            if curr_node.left:
+                stack.append((curr_node.left,dpt+1))
+            if curr_node.right:
+                stack.append((curr_node.right,dpt+1))
+            
+        return max_dpt
