9112903028ab08103a23d9ac445ce0619d657650
Andre Ramos da Silva <andre-ramos1994@hotmail.com>
2023-10-17 16:30:00 -0300

    Time: 1164 ms (8.69%), Space: 74.9 MB (25.79%) - LeetHub

---
+++ 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
@@ -0,0 +1,26 @@
+class Solution:
+    def minReorder(self, n: int, connections: List[List[int]]) -> int:
+        edges = {(a,b) for a,b in connections}
+        neighbors = {city:[]for city in range(n)}
+        visit = set()
+        changes = 0
+
+        for a,b in connections:
+            neighbors[a].append(b)
+            neighbors[b].append(a)
+
+        def dfs(city):
+            nonlocal edges,neighbors,visit,changes
+
+            for neighbor in neighbors[city]:
+                if neighbor in visit:
+                    continue
+                
+                if (neighbor,city) not in edges:
+                    changes+=1
+                visit.add(neighbor)
+                dfs(neighbor)
+        visit.add(0)
+        dfs(0)
+        return changes
+            
